b42564747bcc961cb4f0e9777a383875875e8df1
Douglas Engratulis <dengratulis@gmail.com>
2024-09-07 13:08:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		Locale.setDefault(Locale.US);
+		Department obj = new Department(1,"Books");
+		Seller seller = 
+				new Seller(21,"Dante","dante@gmail.com", new Date(),1500.00,obj);
+		System.out.println(seller);
 	}
 
 }
