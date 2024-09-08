5cbae75145403863a839b5166e4f064be2ad5762
Douglas Engratulis <dengratulis@gmail.com>
2024-09-08 10:01:13 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,21 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Seller;
+import model.entities.impl.SellerDaoJDBC;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
 		Locale.setDefault(Locale.US);
-		Department obj = new Department(1,"Books");
-		Seller seller = 
-				new Seller(21,"Dante","dante@gmail.com", new Date(),1500.00,obj);
+	
+		SellerDAO sellerDao = DaoFactory.createSellerDAO();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.entities.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDAO createSellerDAO() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 }
